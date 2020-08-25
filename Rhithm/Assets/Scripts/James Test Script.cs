2634cc985e062cd738923ba1c40fc3279b1be691
James Milner <jmilner374@gmail.com>
2020-08-26 10:53:00 +1200

    James' Changes
    Added 2 Folders, Audio Files, and a Blue Cube

---
+++ Rhithm/Assets/Scripts/James Test Script.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JamesTestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //If you can see this, we love Nicholas Cage
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

e7c83acee77a5e4cd8310a8637c9ef7b8d214d2a
Rafael <ramos.rafael42@gmail.com>
2020-09-03 15:42:53 +1200

    Added song list UI
    3 songs
    previous and next song functions
    preview each song function
    Name of each song
    Song object
    
    Next: play song advance to mainGameplay scene

---
+++ Rhithm/Assets/Scripts/SongScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SongScript : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+    public string audioName;
+    public float audioLength;
+    public Text audioTextName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioClip = audioSource.clip;
+        audioLength = audioClip.length;
+        audioName = audioClip.name;
+        audioTextName.text = audioName;
+    }
+
+    public void OnClickPlaySong()
+    {
+        audioSource.Play();
+
+        audioSource.SetScheduledEndTime(AudioSettings.dspTime + (10)); //Play for 10 seconds from 0 seconds
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

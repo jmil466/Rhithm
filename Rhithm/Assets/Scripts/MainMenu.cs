e45514adcfcef655235c8e27432e2f0365f4375d
xtd9255 <68973708+JoskyNZ@users.noreply.github.com>
2020-09-17 19:00:49 +1200

    MusicVolumeObject

--- Rhithm/Assets/Scripts/MainMenu.cs
+++ Rhithm/Assets/Scripts/MainMenu.cs
@@ -5,8 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    //public float musicVolValue { get; set; }
+    //public float musicVolValue;
+    public GameObject musicVolObj;
+
     public void Play()
     {
+        DontDestroyOnLoad(musicVolObj);
+
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
---
+++ Rhithm/Assets/Scripts/MusicVolObj.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicVolObj : MonoBehaviour
+{
+    public Slider musicSlider;
+    public float musicVolValue;
+     
+
+    void Update()
+    {
+        musicVolValue = musicSlider.value;
+    }
+
+    public float getMusicVolValue()
+    {
+        return musicVolValue;
+    }
+}

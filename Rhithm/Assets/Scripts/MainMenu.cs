bf4bc91293c9b95a380e94b5caf1fe401259f131
Rafael <ramos.rafael42@gmail.com>
2020-09-22 13:50:50 +1200

    Major Fixes

--- Rhithm/Assets/Scripts/MainMenu.cs
+++ Rhithm/Assets/Scripts/MainMenu.cs
@@ -1,12 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    //public float musicVolValue { get; set; }
+    //public float musicVolValue;
+    public GameObject musicVolObj;
+
     public void Play()
     {
+        MusicVolObj MusicVolObjScript = musicVolObj.GetComponent<MusicVolObj>();
+
+        DontDestroyOnLoad(musicVolObj);
+
+        MusicVolObjScript.musicSlider = null;
+
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
---
+++ Rhithm/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer musicMixer;
+    public AudioMixer sfxMixer;
+
+    public void SetMusicVolume(float volume)
+    {
+        musicMixer.SetFloat("volume", volume);
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        sfxMixer.SetFloat("volume", volume);
+    }
+}

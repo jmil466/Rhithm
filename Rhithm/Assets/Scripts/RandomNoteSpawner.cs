db768d536ea27efa075c09f3d03e87d139ada957
James Milner <jmilner374@gmail.com>
2020-09-24 14:03:15 +1200

    Adjusted Camera + Spawn Time of Blocks
    - Changed position of Main Gameplay Camera
    - Changed the "Stop spawning" time of the RandomNoteSpawner from 55f to 2.5f

--- Rhithm/Assets/Scripts/RandomNoteSpawner.cs
+++ Rhithm/Assets/Scripts/RandomNoteSpawner.cs
@@ -82,7 +82,7 @@ public class RandomNoteSpawner : MonoBehaviour
     {
         yield return new WaitForSeconds(startDelay);
 
-        while (songLength - currentPlayedTime > 55f) // Stops spawning with 2.5s of song remaining
+        while (songLength - currentPlayedTime > 2.5f) // Stops spawning with 2.5s of song remaining
         {
 
             float randomNum = UnityEngine.Random.Range(0.0f, 1.0f);

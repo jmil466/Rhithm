6eec6a6dcfe1d612618c0f2125c6c95ef4835c54
James Milner <jmilner374@gmail.com>
2020-09-25 16:50:29 +1200

    SaveScore Data
    - Created SaveSongData script

--- Rhithm/Assets/Scripts/Score.cs
+++ Rhithm/Assets/Scripts/Score.cs
@@ -15,6 +15,8 @@ public class Score : MonoBehaviour
 
     public Text scoreText;
     public Text multiplierText;
+    public SaveSongData songData;
+
 
     void Start()
     {
@@ -98,6 +100,7 @@ public class Score : MonoBehaviour
         Debug.Log("Current score: " + score + ", High Score was: " + highScore);
         if(score > highScore)
         {
+            songData.saveHighScore(score);
             highScore = score;
             return score;
 

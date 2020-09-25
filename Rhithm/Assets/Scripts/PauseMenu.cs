2df3b6e7f20841cb9bb85860376d6987765761b0
Rafael <ramos.rafael42@gmail.com>
2020-09-25 13:33:32 +1200

    Mute button on SongList and MainGameplay

--- Rhithm/Assets/Scripts/PauseMenu.cs
+++ Rhithm/Assets/Scripts/PauseMenu.cs
@@ -7,6 +7,7 @@ public class PauseMenu : MonoBehaviour
 {
     public GameObject pauseMenu;
     public SongObjectScript song;
+    public AudioSource buttonClickSound;
 
     void Start()
     {
@@ -43,5 +44,18 @@ public class PauseMenu : MonoBehaviour
     public void Exit()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void OnClickMute()
+    {
+        buttonClickSound.mute = !buttonClickSound.mute;
+
+        GameObject songObject = GameObject.Find("SongObject");
+
+        SongObjectScript songObjectScript = songObject.GetComponent<SongObjectScript>();
+
+        AudioSource currentSong = songObjectScript.GetAudioSource();
+
+        currentSong.mute = !currentSong.mute;
     }
 }

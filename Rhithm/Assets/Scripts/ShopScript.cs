7af7caa39ef6de1eb851dcd0235ee3b6d305ef5c
xtd9255 <68973708+JoskyNZ@users.noreply.github.com>
2020-10-08 15:55:20 +1300

    Update ShopScript

--- Rhithm/Assets/Scripts/ShopScript.cs
+++ Rhithm/Assets/Scripts/ShopScript.cs
@@ -5,8 +5,19 @@ using UnityEngine.SceneManagement;
 public class ShopScript : MonoBehaviour
 {
     //Tag shop items Item then use tag to store all items into array
+    public GameObject[] items;
+    //items = GameObject.FindGameObjectsWithTag("Item");
     public Text currencyText;
 
+    void Start()
+    {
+        SetShop();
+    }
+
+    public void SetShop()
+    {
+        items = GameObject.FindGameObjectsWithTag("Item");
+    }
     public void Back()
     {
         SceneManager.LoadScene("MainMenu");
